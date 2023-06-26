74e71bdd18ee43b2cd2cf8098666fcf178c37f53
Ksu Er <kseniapodguzova@gmail.com>
2023-06-26 10:21:16 +0300

    Starting point of the project

---
+++ src/main/java/ru/javaops/restauranvotingapp/RestaurantVotingAppApplication.java
@@ -0,0 +1,13 @@
+package ru.javaops.restauranvotingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantVotingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantVotingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/javaops/restauranvotingapp/RestaurantVotingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.javaops.restauranvotingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantVotingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
